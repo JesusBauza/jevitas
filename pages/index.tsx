abd82efbaf42900efd10286b7f312391517155aa
Juan Villacorta <jvillacorta@norkut.com.ve>
2021-10-19 15:05:32 -0400

    Tiendita

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { PageWithLayout } from '@/components/page'
 import Banner from '@www/home/banner'
 import Banner2 from '@www/home/banner2'
 import Hero from '@www/home/hero'
+import Tiendita from '@www/home/tiendita'
 import Welcome from '@www/home/welcome'
 
 const Index: PageWithLayout = () => {
@@ -9,6 +10,7 @@ const Index: PageWithLayout = () => {
     <>
       <Hero />
       <Welcome />
+      <Tiendita />
       <Banner />
       <Banner2 />
     </>
