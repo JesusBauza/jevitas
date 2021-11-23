6929ad219ce7f005ca3bdc7896836535bff92630
Juan Manuel Villacorta Campos <juanvillacortac@gmail.com>
2021-11-23 19:47:22 +0000

    ...

--- components/page/navigation/footer/index.tsx
+++ components/page/navigation/footer/index.tsx
@@ -18,19 +18,39 @@ const links = [
     titulo: 'De intensidades y dudas',
     href: 'https://anchor.fm/jevitasintensas',
   },
+  {
+    titulo: 'Recursos',
+    href: '/recursos',
+  },
+  {
+    titulo: 'Blog',
+    href: '/blog',
+  },
+  {
+    titulo: 'Nosotras',
+    href: '/nosotras',
+  },
+  {
+    titulo: 'Comunidad de expertas',
+    href: '/citas',
+  },
 ]
 
 const Footer = () => (
-  <footer className="w-full bg-[#FDD2A7] py-24">
+  <footer className="w-full bg-[#FDD2A7] py-24 flex flex-col space-y-16 lg: space-y-10">
     <div className="c-lg flex flex-col lg:flex-row items-center">
-      <div className="w-full justify-center flex lg:justify-start">
+      <div className="w-full items-center flex lg:items-start flex-col space-y-8">
         <LogoSVG className="w-1/2" />
+        <p className="text-sm font-bold text-fg-primary hidden lg:block">Made By Marketing Shakers</p>
       </div>
-      <div className="w-full justify-center flex flex-col space-y-4 mt-16 lg:mt-0">
+      <div className="w-full justify-center grid grid-cols-1 sm:grid-cols-2 sm:grid-rows-4 gap-4 mt-16 lg:mt-0 font-bold">
         {links.map((l, idx) => (
           <CanonicalLink href={l.href} key={idx} className="uppercase hover:underline text-sm text-fg-primary">{l.titulo}</CanonicalLink>
         ))}
       </div>
+    </div>
+    <div className="c-lg lg:hidden">
+        <p className="text-sm font-bold">Made By Marketing Shakers</p>
     </div>
   </footer>
 )
