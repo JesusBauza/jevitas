5bf52dcb0bdf0db053df65848e3a16dfe356dc5f
Juan Manuel Villacorta Campos <juanvillacortac@gmail.com>
2021-12-16 18:39:23 +0000

    DatoCMS

--- components/www/home/banner2.tsx
+++ components/www/home/banner2.tsx
@@ -22,7 +22,7 @@ const Banner2 = () => {
         >
           <div className="flex flex-col space-y-8 w-full lg:w-2/3 py-8" style={setAnim({ y: '0.5rem' })}>
             <h1 className="animate text-3xl xl:text-5xl font-title text-white" dangerouslySetInnerHTML={{
-          __html: banners?.bannerB?.replaceAll('\n', '<br class="hidden lg:block" />')
+          __html: banners?.bannerB?.replace(/\n/g, '<br class="hidden lg:block" />')
         }} />
             <div className="animate" style={setAnim({ d: '100ms', y: '0.5rem' })}>
               <Button href={banners?.bannerBLink} type="vintage" title="Únete a la comunidad" className="uppercase mr-auto" />
