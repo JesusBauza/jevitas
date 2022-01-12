51133e3492d27ee429b3fe8108e0aedaabad9689
Juan Manuel Villacorta Campos <juanvillacortac@gmail.com>
2022-01-12 18:56:53 +0000

    ...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,6 +28,7 @@ const App = ({ Component, pageProps }: Props) => {
       }}
     >
       <Script
+        id="mcjs"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/0f8816d91eeead9aad437ab72/a2a887194376e91ce041dbb79.js");`,
