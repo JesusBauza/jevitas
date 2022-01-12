01b0efbc917321b5152ff77a4ebf5daadcd5018f
Juan Manuel Villacorta Campos <juanvillacortac@gmail.com>
2022-01-12 19:07:08 +0000

    ...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import Page, { PageProps, PageWithLayout } from '@/components/page'
 import { useMemo } from 'react'
 import { SWRConfig } from 'swr'
 import { serviceFetcher } from '@/lib/fetcher'
-import Script from 'next/script'
+import Head from 'next/head'
 
 type Props = AppProps & {
   Component: PageWithLayout
@@ -27,13 +27,14 @@ const App = ({ Component, pageProps }: Props) => {
         }
       }}
     >
-      <Script
-        id="mcjs"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/0f8816d91eeead9aad437ab72/a2a887194376e91ce041dbb79.js");`,
-        }}
-      />
+      <Head>
+        <script
+          id="mcjs"
+          dangerouslySetInnerHTML={{
+            __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/0f8816d91eeead9aad437ab72/a2a887194376e91ce041dbb79.js");`,
+          }}
+        />
+      </Head>
       <CaravaggioProvider url='/api/caravaggio'>
         <Page {...pageProps} {...layoutProps}>
           <Component {...pageProps} />
