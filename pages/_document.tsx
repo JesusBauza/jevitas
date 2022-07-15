8f9acf0f175a1d0b923f08eed575d62e1a21e65e
Juan Manuel Villacorta Campos <37244056+juanvillacortac@users.noreply.github.com>
2022-07-15 14:33:16 -0400

    ...

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,12 +10,12 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-6DBQ695085"></script>
-          <script>{`
+          <script dangerouslySetInnerHTML={{__html: `
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-6DBQ695085');`}</script>
+  gtag('config', 'G-6DBQ695085');`}} />
         </Head>
         <body>
           <script>0</script>
