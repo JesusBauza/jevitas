dbac90d526628cc72641201e2887ff0212cae4c0
Juan Manuel Villacorta Campos <37244056+juanvillacortac@users.noreply.github.com>
2022-07-15 14:20:04 -0400

    ...

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,13 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Montserrat:wght@300;400;500;700;900&display=swap`"
             rel="stylesheet"
           />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-6DBQ695085"></script>
+          <script>{`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6DBQ695085');`}</script>
         </Head>
         <body>
           <script>0</script>
